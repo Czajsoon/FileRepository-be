0592dd2a2ed506c27064bca77f74858124091398
Jakub Czajkowski <kubaczajkowski25@gmail.com>
2022-03-13 01:50:46 +0100

    Initial commit

---
+++ File-Repository/src/main/java/psk/project/FileRepository/FileRepositoryApplication.java
@@ -0,0 +1,13 @@
+package psk.project.FileRepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileRepositoryApplication.class, args);
+	}
+
+}
---
+++ File-Repository/src/test/java/psk/project/FileRepository/FileRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package psk.project.FileRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
